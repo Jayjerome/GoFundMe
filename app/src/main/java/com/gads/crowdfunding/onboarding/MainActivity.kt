a2496da92ffd5545cf6224a129010723861e91c3
collinsturi <cmachuka70@gmail.com>
2022-11-15 19:10:51 +0300

    creation of Log in fragment

--- app/src/main/java/com/gads/crowdfunding/onboarding/MainActivity.kt
+++ app/src/main/java/com/gads/crowdfunding/onboarding/MainActivity.kt
@@ -3,11 +3,9 @@ package com.gads.crowdfunding.onboarding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import com.gads.crowdfunding.R
 import com.gads.crowdfunding.databinding.ActivityMainBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
