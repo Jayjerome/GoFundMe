d53014117be9c67f398e948277f56fd888fb56d2
collinsturi <cmachuka70@gmail.com>
2022-12-05 17:20:54 +0300

    Data manager update

--- app/src/main/java/com/gads/crowdfunding/presentation/dashboard/recyclerviewtypes/DataManager.kt
+++ app/src/main/java/com/gads/crowdfunding/presentation/dashboard/recyclerviewtypes/DataManager.kt
@@ -17,47 +17,47 @@ object DataManager {
 
         cards.add(0, TypeItemDashBoad(0,
                             R.drawable.example_image,
-                            "Urgently - needed",
-                            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
-            35000,
-            20000,
-            3)
+                            "Church Building",
+                            "We kindly ask for your help in funds that would assist us build at a new location",
+            350000,
+            10000,
+            30)
         )
         cards.add(1, TypeItemDashBoad(1,
             R.drawable.example_image,
-                            "Urgently - needed",
-                            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
-            35000,
-            20000,
-            3
+                            "Sick Relative",
+                            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
+            100000,
+            60000,
+            12
         )
         )
         cards.add(2, TypeItemDashBoad(2,
             R.drawable.example_image,
-                            "Urgently - needed",
-                            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
+                            "Orphanage visit",
+                            "We are about to make a visit to a nearby orphanage. We kindly ask for you to donate anything to the address provided or funds",
 
-            35000,
             20000,
-            3
+            13000,
+            7
         )
         )
         cards.add(3, TypeItemDashBoad(3,
             R.drawable.example_image,
-                            "Urgently - needed",
-                            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
-                            35000,
-                          20000,
-                               3
+                            "Hospital expansion",
+                            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
+                            1250000,
+                          670000,
+                               67
         )
         )
         cards.add(4, TypeItemDashBoad(4,
             R.drawable.example_image,
-                            "Urgently - needed",
-                            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
-            35000,
-            20000,
-            3
+                            "School Kitchen needed",
+                            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
+            69000,
+            34100,
+            26
         )
         )
 
@@ -69,42 +69,42 @@ object DataManager {
         itemFundraising.add(0, TypeItemFundraising(0,
                             R.drawable.example_image2,
                             "Charles Chillwel",
-                            30000)
+                            3000)
         )
         itemFundraising.add(1, TypeItemFundraising(1,
             R.drawable.example_image2,
-                            "Charles Chillwel",
-                            30000)
+                            "Jacob",
+                            67000)
         )
         itemFundraising.add(2, TypeItemFundraising(2,
             R.drawable.example_image2,
-                            "Charles Chillwel",
-                            30000)
+                            "Peter Griffin",
+                            3800)
         )
         itemFundraising.add(3, TypeItemFundraising(3,
             R.drawable.example_image2,
-                            "Charles Chillwel",
-                            30000)
+                            "Ruby Margret ",
+                            2048)
         )
         itemFundraising.add(4, TypeItemFundraising(4,
             R.drawable.example_image2,
-                            "Charles Chillwel",
-                            30000)
+                            "James Barnabas",
+                            4930)
         )
         itemFundraising.add(5, TypeItemFundraising(5,
             R.mipmap.ic_launcher,
-            "Charles Chillwel",
-            30000)
+            "West Keith",
+            38903)
         )
         itemFundraising.add(6, TypeItemFundraising(6,
             R.drawable.example_image2,
-            "Charles Chillwel",
-            30000)
+            "Tony Star",
+            24945)
         )
         itemFundraising.add(7, TypeItemFundraising(7,
             R.mipmap.ic_launcher,
-            "Charles Chillwel",
-            30000)
+            "George Brian",
+            29456)
         )
 
 
@@ -115,8 +115,8 @@ object DataManager {
 
         projects.add(0, TypeItemProjects(0,
             R.drawable.example_image,
-            "Urgently - needed",
-            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
+            "Hospital Expansion",
+            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
             35000,
             20000,
             3,
@@ -124,8 +124,8 @@ object DataManager {
         )
         projects.add(1, TypeItemProjects(1,
             R.drawable.example_image,
-            "Urgently - needed",
-            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
+            "School Kitchen Needed",
+            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
             35000,
             20000,
             3,
@@ -134,8 +134,8 @@ object DataManager {
         )
         projects.add(2, TypeItemProjects(2,
             R.drawable.example_image,
-            "Urgently - needed",
-            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
+            "Church Building",
+            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
 
             35000,
             20000,
@@ -145,8 +145,8 @@ object DataManager {
         )
         projects.add(3, TypeItemProjects(3,
             R.drawable.example_image,
-            "Urgently - needed",
-            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
+            "Orphanage Visiting",
+            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
             35000,
             20000,
             3,
@@ -155,8 +155,8 @@ object DataManager {
         )
         projects.add(4, TypeItemProjects(4,
             R.drawable.example_image,
-            "Urgently - needed",
-            "Urgent! For ophanage children xyz xyx vsdo ivsv sv sdoiv asvois dnvos;div svi; asvoi; asvsdvsdhjvb sdv sdv sidlvsd",
+            "Sick Relative",
+            "Lorem ipsum dolor sit amet. Qui earum saepe et quod omnis hic fuga obcaecati sit voluptate aperiam. Ut quia labore non harum itaque ut maiores voluptas hic dolor ratione. Ea expedita accusantium ea architecto illum et quia molestias quo molestiae inventore.",
             35000,
             20000,
             3,
