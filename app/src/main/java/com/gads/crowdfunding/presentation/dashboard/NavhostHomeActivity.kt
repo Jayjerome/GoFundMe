5203cc1ef81c09aeacb2d7e5a9cb758cc885d093
collinsturi <cmachuka70@gmail.com>
2022-12-05 12:51:18 +0300

    Added AndroidEntryPoint annotation to NavhostHomeActivity.kt

--- app/src/main/java/com/gads/crowdfunding/presentation/dashboard/NavhostHomeActivity.kt
+++ app/src/main/java/com/gads/crowdfunding/presentation/dashboard/NavhostHomeActivity.kt
@@ -2,15 +2,16 @@ package com.gads.crowdfunding.presentation.dashboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.gads.crowdfunding.R
 import com.gads.crowdfunding.databinding.ActivityNavHostHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NavhostHomeActivity: AppCompatActivity() {
-    private lateinit var binding : ActivityNavHostHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNavHostHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_nav_host_home)
 
     }
 
